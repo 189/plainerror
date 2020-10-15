d81e46fccb7b55552123deba37b28a93e26a241b
wangshangwen <wangshangwen>
2020-10-15 18:51:06 +0800

    example

--- example/index.js
+++ example/index.js
@@ -1,21 +1,21 @@
 const plainError = require("../lib").default;
 
 function asyncCall(args) {
-    return new Promise((resolve, reject) => {
-        const random = Math.random();
-        if (random < 0.5) {
-            return reject("error");
-        }
-        return resolve(args.name + " got " + random);
-    })
+  return new Promise((resolve, reject) => {
+    const random = Math.random();
+    if (random < 0.5) {
+      return reject("error");
+    }
+    return resolve(args.name + " got " + random);
+  });
 }
 
 (async function () {
-    const [err, response] = await plainError(asyncCall)({ name: "ff" });
-    if (err) {
-        // do something
-        console.log(err);
-        return;
-    }
-    console.log(response);
+  const [err, response] = await plainError(asyncCall)({ name: "lucy" });
+  if (err) {
+    // do something
+    console.log(err);
+    return;
+  }
+  console.log(response);
 })();
