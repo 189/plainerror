1492079a646d8d0b27466427771c48f17b316752
wangshangwen <wangshangwen>
2021-04-29 11:32:40 +0800

    add new type for error params

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 export type AsyncCaller<P> = (...args: any[]) => Promise<P>;
 
-export default function plainError<T>(caller: AsyncCaller<T>, context?: any) {
-    return function wrapped(...args: any[]): Promise<[Error | null, T]> {
-        return (caller.apply(context, args) as Promise<T>).then(
-            (res: T) => {
-                return [null, res] as [null, T];
-            },
-            (err: Error) => {
-                return [err, {}] as [Error, T];
-            }
-        );
-    };
+export default function plainError<T, E = {}>(
+  caller: AsyncCaller<T>,
+  context?: any
+) {
+  return function wrapped(...args: any[]): Promise<[Error | null, T]> {
+    return (caller.apply(context, args) as Promise<T>).then(
+      (res: T) => {
+        return [null, res] as [null, T];
+      },
+      (err: Error & E) => {
+        return [err, {}] as [Error & E, T];
+      }
+    );
+  };
 }
