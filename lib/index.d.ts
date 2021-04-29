009cc3d0b3cc131d771c68125cc548fdcfbff71e
wangshangwen <wangshangwen>
2021-04-29 11:40:13 +0800

    retype error

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,3 @@
 export declare type AsyncCaller<P> = (...args: any[]) => Promise<P>;
-export default function plainError<T, E = {}>(caller: AsyncCaller<T>, context?: any): (...args: any[]) => Promise<[Error | null, T]>;
+export declare type CustomError<E> = Error & E;
+export default function plainError<T, E = {}>(caller: AsyncCaller<T>, context?: any): (...args: any[]) => Promise<[CustomError<E> | null, T]>;
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 export type AsyncCaller<P> = (...args: any[]) => Promise<P>;
-
+export type CustomError<E> = Error & E;
 export default function plainError<T, E = {}>(
   caller: AsyncCaller<T>,
   context?: any
 ) {
-  return function wrapped(...args: any[]): Promise<[Error | null, T]> {
+  return function wrapped(...args: any[]): Promise<[CustomError<E> | null, T]> {
     return (caller.apply(context, args) as Promise<T>).then(
       (res: T) => {
         return [null, res] as [null, T];
       },
-      (err: Error & E) => {
-        return [err, {}] as [Error & E, T];
+      (err: CustomError<E>) => {
+        return [err, {}] as [CustomError<E>, T];
       }
     );
   };
